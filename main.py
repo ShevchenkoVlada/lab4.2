32729b5f00aede8f6da8c8bf93e556e018091fc0
unknown <shevchenkovlada623@gmail.com>
2024-02-27 11:18:05 +0200

    third commit

--- main.py
+++ main.py
@@ -3,3 +3,12 @@ if is_prime(number_to_check):
     print(f"{number_to_check} - просте число")
 else:
     print(f"{number_to_check} - не є простим числом")
+<<<<<<< Updated upstream
+=======
+
+result = is_power_of_2(16)
+print(result)
+
+result = is_power_of_5(25)
+print(result)  
+>>>>>>> Stashed changes
